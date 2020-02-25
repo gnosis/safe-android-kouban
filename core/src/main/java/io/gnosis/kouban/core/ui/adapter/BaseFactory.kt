09859430fdb894df027205eca5048bdac412b91b
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-02-25 12:09:14 +0100

    Changed implementation to support multiple viewHolders in single adapter

--- core/src/main/java/io/gnosis/kouban/core/ui/adapter/BaseFactory.kt
+++ core/src/main/java/io/gnosis/kouban/core/ui/adapter/BaseFactory.kt
@@ -4,10 +4,11 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFactory<VB, out VH>
-        where VB : ViewBinding, VH : BaseViewHolder<*, VB> {
+abstract class BaseFactory<VH : BaseViewHolder<Any>> {
 
-    abstract fun newViewHolder(viewBinding: VB, viewType: Int): VH
+    abstract fun newViewHolder(viewBinding: ViewBinding, viewType: Int): VH
 
-    abstract fun layout(layoutInflater: LayoutInflater, parent: ViewGroup, viewType: Int): VB
+    abstract fun layout(layoutInflater: LayoutInflater, parent: ViewGroup, viewType: Int): ViewBinding
+
+    open fun <T> viewTypeFor(item: T): Int = 0
 }
