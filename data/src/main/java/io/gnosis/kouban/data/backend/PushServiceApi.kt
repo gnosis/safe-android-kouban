d96abde2a5e49c17d6d28be86c224954d740bb18
Vitaly Katz <vitaly@gnosis.io>
2020-03-05 16:19:26 +0100

    Add push service api

---
+++ data/src/main/java/io/gnosis/kouban/data/backend/PushServiceApi.kt
@@ -0,0 +1,39 @@
+package io.gnosis.kouban.data.backend
+
+import com.squareup.moshi.Json
+import io.gnosis.kouban.data.BuildConfig
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface PushServiceApi {
+    @POST("1/observers")
+    suspend fun registerDevice(@Body deviceRegistration: DeviceRegistration)
+
+    @POST("1/{network}/1/safes/{safe_address}/observe")
+    suspend fun registerPushes(
+        @Path("network") network: String,
+        @Path("safe_address") safeAddress: String,
+        @Body pushesRegistration: PushesRegistration
+    )
+
+    data class DeviceRegistration(
+        @Json(name = "client_id")
+        val clientId: String,
+        @Json(name = "push_token")
+        val pushToken: String
+    )
+
+    data class PushesRegistration(
+        @Json(name = "client_id")
+        val clientId: String
+    )
+
+    companion object {
+
+        const val BASE_URL = BuildConfig.PUSH_SERVICE_URL
+    }
+}
+
+
