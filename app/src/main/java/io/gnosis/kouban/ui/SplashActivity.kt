bdbc5d00ccfb4dd2332c31665444a8bdfeede01d
Vitaly Katz <vitaly@gnosis.io>
2020-03-10 09:32:21 +0100

    Add splash screen

---
+++ app/src/main/java/io/gnosis/kouban/ui/SplashActivity.kt
@@ -0,0 +1,16 @@
+package io.gnosis.kouban.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 500)
+    }
+}
